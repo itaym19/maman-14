93832ddb410592815ec32a356595caeb053d482f
itaym19 <openu1111@gmail.com>
2025-02-27 14:58:20 +0200

    Create handleCommand.c

---
+++ handleCommand.c
@@ -0,0 +1,12 @@
+/* function for trimming leading and trailing spaces */
+char *trim_white_spaces(char *s) {
+    char *end;
+    while( isspace(*s) ) s++;  /* Trim leading space */
+    if(*s == 0) return s;  /* all white spaces */ 
+    /* Trim trailing space */
+    end = s + strlen(s) - 1;
+    while( end > s && isspace(*end) ) end--;
+    end[1] = '\0';  /* Write new null terminator character */
+    
+    return s;
+}
