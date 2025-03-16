4579532e9f08b8fc9dd755660f7937e7ef49397d
itaym19 <openu1111@gmail.com>
2025-03-16 15:47:43 +0200

    Update handleCommand.c

--- handleCommand.c
+++ handleCommand.c
@@ -16,37 +16,53 @@ bool identify_command(char *line, command *command) {
     int i=0, j=0;
     bool isLastCharComma = false;
     /* go through first word */
-    while ( (!isspace(*line[i]) ) && (*line[i] != '\0' ) { 
+    while ( (!isspace(line[i]) ) && (line[i] != '\0' ) { 
         i++
     }
     /* copy first word to command's name */
-    for(j = 0; j < i; j++) {
-        if (j <= MAX_COMMAND_LENGTH) {
-            (command -> commandName[j]) = *line[j];
-        }
-        (command -> commandName[j+1]) = '\0';
+    if ( (i-j) > MAX_COMMAND_LENGTH) {
+        strncpy(command->commandName, line[j], MAX_COMMAND_LENGTH + 1);
     }
-    /* go to start of second word */
-    while (isspace(*line[i])) {
+    else {
+        strncpy(command->commandName, line[j], i-j + 1);
+    }
+    /* advance j to reach i */
+    for (j<i; j++); 
+    
+    /* go to start of second the parameters section */
+    while ( (isspace(line[i])) && (line[i] != '\n') ) {
         i++;
         j++;
     }
     
-    if (*line[i] == ",") {
+    if (line[i] == ",") {
         fprintf(stderr, "error: there is a comma before first parameter");
         return false;
     }
+    
+    if (line[i] == "\n") return true;
+    
     /* go through second word */
-    while ( (!isspace(*line[i] && (*line[i] != ',') && (*line[i] != '\0' ) ) {
+    while ( (!isspace(line[i]))  && (line[i] != ',') && (line[i] != '\0') && (line[i] != '\n') ) {
         i++;
     }
     /* copy second word to first parameter */
-    for( ; j < i; j++) {
-        if (j <= MAX_LABEL_LENGTH) {
-            (command -> commandName[j]) = *line[j];
-        }
-        (command -> commandName[j+1]) = '\0';
+    if ( (i-j) > MAX_LABEL_LENGTH) {
+        strncpy(command->firstParam, line[j], MAX_LABEL_LENGTH + 1);
     }
+    else {
+        strncpy(command->firstParam, line[j], i-j + 1);
+    }
+    /* advance j to reach i */
+    for (j<i; j++);
+    
+    if (line[i] == "\n") return true;
+    /* skip optional spaces between parameters */
+    while ( (isspace(line[i])) && (line[i] != '\n') ) {
+        i++;
+        j++;
+    }
+    
     
 }
     
